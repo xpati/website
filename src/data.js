5187bed90ffaba24fc6b42879dd1bfdce3ea716a
Sara Tranquada <sara.tranquada@nearsoftsolutions.com>
2025-04-20 13:03:28 +0100

    upate language and skills

--- src/data.js
+++ src/data.js
@@ -1,49 +1,83 @@
 const skillsList = [
   { title: "React JS", value: 75 },
+  { title: "React Native", value: 75 },
+  { title: "React Navigation", value: 75 },
+  { title: "TypeScript", value: 75 },
+  { title: "Redux ", value: 60 },
   { title: "HTML", value: 80 },
-  { title: "JavaScript", value: 80 },
-  { title: "CSS", value: 80 },
+  { title: "Figma ", value: 60 },
+  { title: "CSS3", value: 80 },
   { title: "Bulma ", value: 60 },
+  { title: "TailwindCSS ", value: 60 },
+  { title: "JavaScript (ES6+)", value: 80 },
+  { title: "Svelte ", value: 60 },
+  { title: "D3.js ", value: 60 },
   { title: "Bootstrap", value: 70 },
+  { title: "Python", value: 70 },
+  { title: "PHP", value: 70 },
+  { title: "HTML5", value: 70 },
+  { title: "WordPress", value: 70 },
+  { title: "MySQL", value: 70 },
+  { title: "React Native Animations", value: 75 },
 ];
 const projectList = [
   {
     id: 1,
     title: "Mulheres e a Tecnologia",
-    technologies: ["Book"],
+    technologies: [2022, "Book"],
+    link: "https://www.diariodebordoeditores.pt/livros/mulheres-e-a-tecnologia",
     backgroundImage:
       "https://i.pinimg.com/originals/92/5f/c6/925fc6741e9864c0c082b6e6559aef1a.jpg",
-    frontImage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJKPnHyJHHZVxkZvgw03S-o8eYVv0oX0CSpGO5BTYgPOfjnEt67vs-kbfrV-NsDPbJOWQ&usqp=CAU",
+    frontImage:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJKPnHyJHHZVxkZvgw03S-o8eYVv0oX0CSpGO5BTYgPOfjnEt67vs-kbfrV-NsDPbJOWQ&usqp=CAU",
   },
   {
     id: 2,
-    title: "Clínicas do Dr Joaquim Vieira",
-    technologies: ["Bootstrap 5"],
+    title: "Clínicas do Dr. Joaquim Vieira",
+    link: "https://clinicajv.pt/",
+    technologies: [2022, "Bootstrap 5"],
     backgroundImage:
       "https://static.vecteezy.com/system/resources/previews/002/375/040/original/modern-white-background-free-vector.jpg",
-    frontImage: "https://res.cloudinary.com/dju5xkmvw/image/upload/v1658679493/cjv_cnlqda.jpg",
+    frontImage:
+      "https://res.cloudinary.com/dju5xkmvw/image/upload/v1658679493/cjv_cnlqda.jpg",
   },
   {
     id: 3,
-    title: "Confronting the Numbers of Women in Computer Science & Engineering (CSE): Making the Gender Gap Visible",
-    technologies: ["2021","Phd Digital Media", "Gender Stereotype"],
+    title:
+      "Confronting the Numbers of Women in Computer Science & Engineering (CSE): Making the Gender Gap Visible",
+    link: "https://run.unl.pt/handle/10362/123962",
+    technologies: ["2021", "Phd Digital Media", "Gender Stereotype"],
     backgroundImage:
       "https://img.freepik.com/free-photo/old-black-background-grunge-texture-dark-wallpaper-blackboard-chalkboard-room-wall_1258-28312.jpg",
     frontImage:
       "https://res.cloudinary.com/dju5xkmvw/image/upload/v1657815004/Untitled2_xr6gkw.png",
   },
+  // {
+  //   id: 7,
+  //   title: "LFC",
+  //   link: "http://www.cee.uma.pt/edu/gamedesign/shadowcast/about.html",
+  //   technologies: ["2019", "Android Studio"],
+  //   backgroundImage:
+  //     "https://cdn.cbeditz.com/cbeditz/preview/black-red-gradient-background-wallpaper-74-11614352798fbqrv1wpuv.jpg",
+  //   frontImage:
+  //     "https://res.cloudinary.com/dju5xkmvw/image/upload/v1658679751/team_ivqydh.jpg",
+  // },
   {
     id: 4,
-    title: "Addomini Administracao de Condominios, Lda",
-    technologies: ["Wordpress"],
+    title: "Ad Domini Administracao de Condominios, Lda",
+    link: "https://www.addomini.com/",
+    technologies: [2019, "Wordpress"],
     backgroundImage:
       "https://img.freepik.com/free-vector/dark-gradient-background-with-copy-space_53876-99548.jpg?size=626&ext=jpg&ga=GA1.2.2102900112.1628985600",
-    frontImage: "https://res.cloudinary.com/dju5xkmvw/image/upload/v1657639333/addomini_hmq3wk.png",
+    frontImage:
+      "https://res.cloudinary.com/dju5xkmvw/image/upload/v1657639333/addomini_hmq3wk.png",
   },
   {
     id: 5,
-    title: "Hospital Hero: a game for reducing stress and anxiety of children while waiting in emergency room",
-    technologies: ["2012", "Master Computer Engineer", "Unity"] ,
+    title:
+      "Hospital Hero: a game for reducing stress and anxiety of children while waiting in emergency room",
+    link: "https://www.proquest.com/openview/af5d4cbff79d096c0293c2110424f23c/1?cbl=2026366&diss=y&pq-origsite=gscholar",
+    technologies: ["2014", "Master Computer Engineer", "Unity"],
     backgroundImage:
       "https://static.vecteezy.com/system/resources/previews/002/375/040/original/modern-white-background-free-vector.jpg",
     frontImage:
@@ -52,14 +86,14 @@ const projectList = [
   {
     id: 6,
     title: "Shadow Cast Studios",
+    link: "http://www.cee.uma.pt/edu/gamedesign/shadowcast/about.html",
     technologies: ["2012", "Game Design class", "University of Madeira"],
     backgroundImage:
       "https://cdn.cbeditz.com/cbeditz/preview/black-red-gradient-background-wallpaper-74-11614352798fbqrv1wpuv.jpg",
     frontImage:
-    "https://res.cloudinary.com/dju5xkmvw/image/upload/v1658679751/team_ivqydh.jpg",
+      "https://res.cloudinary.com/dju5xkmvw/image/upload/v1658679751/team_ivqydh.jpg",
   },
 ];
-
 
 const experienceList = [
   {
